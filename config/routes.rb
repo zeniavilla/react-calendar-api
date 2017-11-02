593741cfab396ab94025655f5cf760bdcc8bb115
Zenia <zeniavilla@hotmail.com>
2017-11-02 11:47:02 -0700

    Add appointments/:date route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :appointments, only: [:create, :show, :destroy]
+    resources :appointments, only: [:create, :destroy]
+    get 'appointments/:date', to: 'appointments#find_by_date', as: 'appointment_date', param: :date
   end
 end
